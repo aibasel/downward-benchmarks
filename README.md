1262699e57c62c6fb1223cfb31116c2aaed5607d
Jendrik Seipp <jendrik.seipp@unibas.ch>
2016-04-27 15:25:08 +0200

    add benchmarks from Fast Downward repository

---
+++ README.md
@@ -0,0 +1,14 @@
+Fast Downward benchmark collection
+==================================
+
+An inofficial collection of (sequential) IPC benchmark instances.
+
+suites.py contains many predefined benchmark suites such as
+"optimal_strips", "satisficing", or "all". To obtain the
+corresponding domain list for your experiments you can run
+
+    ./suites.py optimal_strips
+
+Please note that some IPC 2014 domains from the satisificing (sat)
+track were also used in the multicore (mco) and agile (agl) tracks.
+Please consult suites.py for the full domain lists of these two tracks.
---
+++ tests.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+
+from collections import Counter
+import os
+
+import suites
+from suites import *
+
+
+REPO = os.path.abspath(os.path.dirname(__file__))
+
+
+def test_for_duplicates():
+    for funcname in dir(suites):
+        if not funcname.startswith('suite_'):
+            continue
+        print 'Test', funcname
+        func = getattr(suites, funcname)
+        domains = func()
+        assert domains == sorted(domains)
+        assert len(set(domains)) == len(domains), Counter(domains)
+
+
+def test_suite_satisficing():
+    assert (
+        set(suite_satisficing_adl() + suite_satisficing_strips()) ==
+        set(suite_ipc98_to_ipc04() + suite_ipc06() +
+            suite_ipc06_strips_compilations() + suite_ipc08_sat() +
+            suite_ipc11_sat() + suite_ipc14_sat()))
+
+
+def test_suite_optimal():
+    assert (
+        set(suite_optimal_adl() + suite_optimal_strips()) ==
+        set(suite_ipc98_to_ipc04() + suite_ipc06() +
+            suite_ipc06_strips_compilations() + suite_ipc08_opt() +
+            suite_ipc11_opt() + suite_ipc14_opt()))
+
+
+def test_paths_exist():
+    suite_funcs = [
+        getattr(suites, funcname) for funcname in dir(suites)
+        if funcname.startswith("suite_")]
+    for func in suite_funcs:
+        suite = func()
+        for part in suite:
+            path = os.path.join(REPO, part.replace(":", "/"))
+            if not os.path.exists(path):
+                print("Error: {} could not be found for {}.".format(
+                    part, func.__name__))
+
+
+def test_all_domains_covered():
+    dirs = set(
+        path for path in os.listdir(REPO)
+        if os.path.isdir(os.path.join(REPO, path)) and
+        not path.startswith((".", "_")))
+    covered = set(suite_all())
+    assert covered == dirs
+
+
+def test_file_endings():
+    paths = set(
+        os.path.join(REPO, path) for path in sorted(os.listdir(REPO))
+        if os.path.isdir(os.path.join(REPO, path)) and
+        not path.startswith((".", "_")))
+    for path in paths:
+        for filename in sorted(os.listdir(path)):
+            assert filename.endswith(".pddl"), (path, filename)
