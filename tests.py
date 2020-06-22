d6febbba6012806ec92214a00b7f842fe3dacd34
Jendrik Seipp <jendrikseipp@gmail.com>
2020-06-22 16:18:41 +0200

    Set up GitHub actions pipeline. (#3)

--- tests.py
+++ tests.py
@@ -12,6 +12,15 @@ from suites import *
 REPO = os.path.abspath(os.path.dirname(__file__))
 
 
+def _get_domains():
+    return sorted(
+        domain
+        for domain in os.listdir(REPO)
+        if os.path.isdir(os.path.join(REPO, domain)) and
+        not domain.startswith((".", "_")) and
+        domain != "unofficial-reformulations")
+
+
 def test_for_duplicates():
     for funcname in dir(suites):
         if not funcname.startswith('suite_'):
@@ -39,42 +48,15 @@ def test_suite_optimal():
             suite_ipc11_opt() + suite_ipc14_opt() + suite_ipc18_opt()))
 
 
-def test_paths_exist():
-    suite_funcs = [
-        getattr(suites, funcname) for funcname in dir(suites)
-        if funcname.startswith("suite_")]
-    for func in suite_funcs:
-        suite = func()
-        for part in suite:
-            path = os.path.join(REPO, part.replace(":", "/"))
-            if not os.path.exists(path):
-                print("Error: {} could not be found for {}.".format(
-                    part, func.__name__))
-
-
 def test_all_domains_covered():
-    dirs = set(
-        path for path in os.listdir(REPO)
-        if os.path.isdir(os.path.join(REPO, path)) and
-        not path.startswith((".", "_")))
-    covered = set(suite_all())
-    assert covered == dirs
+    assert set(suite_all()) == set(_get_domains())
 
 
 def test_file_endings():
-    paths = set(
-        os.path.join(REPO, path) for path in sorted(os.listdir(REPO))
-        if os.path.isdir(os.path.join(REPO, path)) and
-        not path.startswith((".", "_")))
-    for path in paths:
+    for path in _get_domains():
         for filename in sorted(os.listdir(path)):
             assert filename.endswith(".pddl"), (path, filename)
 
 
 def test_all_domains_in_domain_to_tags():
-    dirs = set(
-        path for path in os.listdir(REPO)
-        if os.path.isdir(os.path.join(REPO, path)) and
-        not path.startswith((".", "_")))
-    domains = set(DOMAIN_TO_TAGS.keys())
-    assert domains == dirs
+    assert set(DOMAIN_TO_TAGS.keys()) == set(_get_domains())
